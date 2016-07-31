b76296e34521f0f1264ed5bbf4da5911d2340e90
Ubuntu <ubuntu@ip-172-31-28-220.eu-central-1.compute.internal>
2016-07-31 16:52:32 +0000

    /frontend

--- app_front.js
+++ app_front.js
@@ -38,9 +38,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/public', proxy("localhost:3000"));
-app.use('/[a-z]', proxy("http://twoseek.tk"));
+app.use('/frontend', routes);
+app.use('/', proxy("twoseek.tk"));
 //app.use('/users', users);
 
 // catch 404 and forward to error handler
